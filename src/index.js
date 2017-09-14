8cfe5438c702cca895fb5e274964d322e7debf04
JahilMag <jahil@magneticcreative.co.za>
2017-09-14 18:17:59 +0200

    React redux added

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import reducer from './reducers/index'
 const store = createStore(
     reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+)
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App store={store}/>, document.getElementById('root'));
 registerServiceWorker();
